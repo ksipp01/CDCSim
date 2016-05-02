a3dc1038dc4170e5cad2191ba3c69b5e4c3eda51
ksipp01 <ksipp911@gmail.com>
2016-05-01 19:20:05 -0500

    First Commit

---
+++ CDC Camera Simulator/Blur.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace ASCOM.SimCDC
+{
+    class Blur
+    {
+        //private void btnLoad_Click(object sender, EventArgs e)
+        //{
+        //    using (OpenFileDialog diag = new OpenFileDialog())
+        //    {
+        //        diag.Filter = "Bitmap|*.bmp;*.jpg;*.gif";
+        //        if (diag.ShowDialog() == DialogResult.OK)
+        //        {
+        //            try
+        //            {
+        //                picOriginal.Image = Image.FromFile(diag.FileName);
+        //            }
+        //            catch (Exception)
+        //            {
+        //                MessageBox.Show("Invalid Image");
+        //            }
+        //        }
+        //    }
+        //}
+      //  int nAmount = 3;  // amount of blur
+        public Bitmap ApplyBlur(Bitmap image, int nAmount)
+        {
+        //    btnApply.Enabled = false;
+         //   this.Cursor = Cursors.WaitCursor;
+            image = FastBoxBlur(image, nAmount);
+          //  picConvolved.Image = FastBoxBlur(picOriginal.Image, (int)nAmount.Value);
+
+          //  this.Cursor = Cursors.Default;
+           // btnApply.Enabled = true;
+            return image;
+        }
+
+        //private void btnApply_Click(object sender, EventArgs e)
+        //{
+        //    if (picOriginal.Image != null)
+        //    {
+        //        btnApply.Enabled = false;
+        //        this.Cursor = Cursors.WaitCursor;
+
+        //        picConvolved.Image = FastBoxBlur(picOriginal.Image, (int)nAmount.Value);
+
+        //        this.Cursor = Cursors.Default;
+        //        btnApply.Enabled = true;
+        //    }
+        //}
+
+        private Bitmap Convolve(Bitmap input, float[,] filter)
+        {
+            //Find center of filter
+            int xMiddle = (int)Math.Floor(filter.GetLength(0) / 2.0);
+            int yMiddle = (int)Math.Floor(filter.GetLength(1) / 2.0);
+
+            //Create new image
+            Bitmap output = new Bitmap(input.Width, input.Height);
+
+            FastBitmap reader = new FastBitmap(input);
+            FastBitmap writer = new FastBitmap(output);
+            reader.LockImage();
+            writer.LockImage();
+
+            for (int x = 0; x < input.Width; x++)
+            {
+                for (int y = 0; y < input.Height; y++)
+                {
+                    float r = 0;
+                    float g = 0;
+                    float b = 0;
+
+                    //Apply filter
+                    for (int xFilter = 0; xFilter < filter.GetLength(0); xFilter++)
+                    {
+                        for (int yFilter = 0; yFilter < filter.GetLength(1); yFilter++)
+                        {
+                            int x0 = x - xMiddle + xFilter;
+                            int y0 = y - yMiddle + yFilter;
+
+                            //Only if in bounds
+                            if (x0 >= 0 && x0 < input.Width &&
+                                y0 >= 0 && y0 < input.Height)
+                            {
+                                Color clr = reader.GetPixel(x0, y0);
+
+                                r += clr.R * filter[xFilter, yFilter];
+                                g += clr.G * filter[xFilter, yFilter];
+                                b += clr.B * filter[xFilter, yFilter];
+                            }
+                        }
+                    }
+
+                    //Normalize (basic)
+                    if (r > 255)
+                        r = 255;
+                    if (g > 255)
+                        g = 255;
+                    if (b > 255)
+                        b = 255;
+
+                    if (r < 0)
+                        r = 0;
+                    if (g < 0)
+                        g = 0;
+                    if (b < 0)
+                        b = 0;
+
+                    //Set the pixel
+                    writer.SetPixel(x, y, Color.FromArgb((int)r, (int)g, (int)b));
+                }
+            }
+
+            reader.UnlockImage();
+            writer.UnlockImage();
+
+            return output;
+        }
+
+        /// <summary>
+        /// Returns a box filter 1D kernel that is in the format {1,..,n}
+        /// </summary>
+        private float[,] GetHorizontalFilter(int size)
+        {
+            float[,] smallFilter = new float[size, 1];
+            float constant = size;
+
+            for (int i = 0; i < size; i++)
+            {
+                smallFilter[i, 0] = 1.0f / constant;
+            }
+
+            return smallFilter;
+        }
+
+        /// <summary>
+        /// Returns a box filter 1D kernel that is in the format {1},...,{n}
+        /// </summary>
+        private float[,] GetVerticalFilter(int size)
+        {
+            float[,] smallFilter = new float[1, size];
+            float constant = size;
+
+            for (int i = 0; i < size; i++)
+            {
+                smallFilter[0, i] = 1.0f / constant;
+            }
+
+            return smallFilter;
+        }
+
+        /// <summary>
+        /// Returns a box filter 2D kernel in the format {1,...,n},...,{1,...,n}
+        /// </summary>
+        private float[,] GetBoxFilter(int size)
+        {
+            float[,] filter = new float[size, size];
+            float constant = size * size;
+
+            for (int i = 0; i < filter.GetLength(0); i++)
+            {
+                for (int j = 0; j < filter.GetLength(1); j++)
+                {
+                    filter[i, j] = 1.0f / constant;
+                }
+            }
+
+            return filter;
+        }
+
+        private Bitmap BoxBlur(Image img, int size)
+        {
+            //Apply a box filter by convolving the image with a 2D kernel
+            return Convolve(new Bitmap(img), GetBoxFilter(size));
+        }
+
+        private Bitmap FastBoxBlur(Image img, int size)
+        {
+            //Apply a box filter by convolving the image with two separate 1D kernels (faster)
+            return Convolve(Convolve(new Bitmap(img), GetHorizontalFilter(size)), GetVerticalFilter(size));
+        }
+
+
+        unsafe public class FastBitmap
+        {
+            private struct PixelData
+            {
+                public byte blue;
+                public byte green;
+                public byte red;
+                public byte alpha;
+
+                public override string ToString()
+                {
+                    return "(" + alpha.ToString() + ", " + red.ToString() + ", " + green.ToString() + ", " + blue.ToString() + ")";
+                }
+            }
+
+            private Bitmap workingBitmap = null;
+            private int width = 0;
+            private BitmapData bitmapData = null;
+            private Byte* pBase = null;
+
+            public FastBitmap(Bitmap inputBitmap)
+            {
+                workingBitmap = inputBitmap;
+            }
+
+            public void LockImage()
+            {
+                Rectangle bounds = new Rectangle(Point.Empty, workingBitmap.Size);
+
+                width = (int)(bounds.Width * sizeof(PixelData));
+                if (width % 4 != 0) width = 4 * (width / 4 + 1);
+
+                //Lock Image
+                bitmapData = workingBitmap.LockBits(bounds, ImageLockMode.ReadWrite, PixelFormat.Format32bppArgb);
+                pBase = (Byte*)bitmapData.Scan0.ToPointer();
+            }
+
+            private PixelData* pixelData = null;
+
+            public Color GetPixel(int x, int y)
+            {
+                pixelData = (PixelData*)(pBase + y * width + x * sizeof(PixelData));
+                return Color.FromArgb(pixelData->alpha, pixelData->red, pixelData->green, pixelData->blue);
+            }
+
+            public Color GetPixelNext()
+            {
+                pixelData++;
+                return Color.FromArgb(pixelData->alpha, pixelData->red, pixelData->green, pixelData->blue);
+            }
+
+            public void SetPixel(int x, int y, Color color)
+            {
+                PixelData* data = (PixelData*)(pBase + y * width + x * sizeof(PixelData));
+                data->alpha = color.A;
+                data->red = color.R;
+                data->green = color.G;
+                data->blue = color.B;
+            }
+
+            public void UnlockImage()
+            {
+                workingBitmap.UnlockBits(bitmapData);
+                bitmapData = null;
+                pBase = null;
+            }
+        }
+
+
+    }
+}
---
+++ CDC Camera Simulator/ScreenCapture.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+
+
+namespace ASCOM.SimCDC
+{
+    class ScreenCapture
+    {
+
+        Camera camera;
+    //    SetupDialogForm sdf = new SetupDialogForm();
+        Blur blr = new Blur();
+     //   SetupDialogForm setup = new SetupDialogForm();
+      //  string fullPath = @"C:\Users\Public\CDC_CamSimulator\CDC Camera Simulator\CDC Camera Simulator";
+      //  string fullPath = @"C:\Users\Public\CDC_CamSimulator\CDC Camera Simulator\CDC Camera Simulator\bin\Debug";
+        string fullPath = Path.GetDirectoryName(SetupDialogForm.CapturePath);
+    //    string fullPath = Path.GetDirectoryName(System.Reflection.Assembly.GetAssembly((this.GetType())).Location);
+        /// <summary>
+        /// Creates an Image object containing a screen shot of the entire desktop
+        /// </summary>
+        /// <returns></returns>
+        public Image CaptureScreen()
+        {
+            return CaptureWindow(User32.GetDesktopWindow());
+        }
+        /// <summary>
+        /// Creates an Image object containing a screen shot of a specific window
+        /// </summary>
+        /// <param name="handle">The handle to the window. 
+        /// (In windows forms, this is obtained by the Handle property)</param>
+        /// <returns></returns>
+        public Image CaptureWindow(IntPtr handle)
+        {
+            // get te hDC of the target window
+            IntPtr hdcSrc = User32.GetWindowDC(handle);
+            // get the size
+            User32.RECT windowRect = new User32.RECT();
+            User32.GetWindowRect(handle, ref windowRect);
+
+
+
+            //   int width = windowRect.right - windowRect.left;
+            //   int height = windowRect.bottom - windowRect.top;
+
+            //int width = 800;
+            //int height = 600;
+            int width = SetupDialogForm.Width;
+            int height = SetupDialogForm.Height;
+
+
+            // create a device context we can copy to
+            IntPtr hdcDest = GDI32.CreateCompatibleDC(hdcSrc);
+            // create a bitmap we can copy it to,
+            // using GetDeviceCaps to get the width/height
+            IntPtr hBitmap = GDI32.CreateCompatibleBitmap(hdcSrc, width, height);
+            // select the bitmap object
+            IntPtr hOld = GDI32.SelectObject(hdcDest, hBitmap);
+            // bitblt over
+            GDI32.BitBlt(hdcDest, 0, 0, width, height, hdcSrc, 500, 300, GDI32.SRCCOPY);
+            // restore selection
+            GDI32.SelectObject(hdcDest, hOld);
+            // clean up
+            GDI32.DeleteDC(hdcDest);
+            User32.ReleaseDC(handle, hdcSrc);
+            // get a .NET image object for it
+            Image img = Image.FromHbitmap(hBitmap);
+            // free up the Bitmap object
+            GDI32.DeleteObject(hBitmap);
+            return img;
+        }
+        /// <summary>
+        /// Captures a screen shot of a specific window, and saves it to a file
+        /// </summary>
+        /// <param name="handle"></param>
+        /// <param name="filename"></param>
+        /// <param name="format"></param>
+
+       private int focusPos = 25000;
+
+        public void CaptureWindowToFile(IntPtr handle, string filename, ImageFormat format)
+        {
+            if (System.IO.File.Exists(filename))
+            {
+                System.IO.File.Delete(filename);
+            }
+
+            Image img = CaptureWindow(handle);
+            Bitmap bmp = (Bitmap)img;
+      //      int pos = setup.focuser.Position;
+
+          //  int pos = SetupDialogForm.focuser.Position;
+         //   int amount = Math.Abs(pos / 100 - focusPos  / 100);
+            if (SetupDialogForm.FocusStepSize != 0)  // first run capture can't use this.  
+            {
+                int amount = Math.Abs(SetupDialogForm.focuser.Position / SetupDialogForm.FocusStepSize - SetupDialogForm.FocusPoint / SetupDialogForm.FocusStepSize);
+                if (amount > 10)
+                    amount = 10;
+                //   if (((focusPos - pos) > 100) || ((pos-focusPos > 100)))
+                if (amount > 0)
+                    img = blr.ApplyBlur(bmp, amount +1);
+            }
+            img.Save(filename, format);
+        }
+        /// <summary>
+        /// Captures a screen shot of the entire desktop, and saves it to a file
+        /// </summary>
+        /// <param name="filename"></param>
+        /// <param name="format"></param>
+        public void CaptureScreenToFile(string filename, ImageFormat format)
+        {
+            Image img = CaptureScreen();
+            img.Save(filename, format);
+        }
+
+        /// <summary>
+        /// Helper class containing Gdi32 API functions
+        /// </summary>
+        private class GDI32
+        {
+
+            public const int SRCCOPY = 0x00CC0020; // BitBlt dwRop parameter
+            [DllImport("gdi32.dll")]
+            public static extern bool BitBlt(IntPtr hObject, int nXDest, int nYDest,
+                int nWidth, int nHeight, IntPtr hObjectSource,
+                int nXSrc, int nYSrc, int dwRop);
+            [DllImport("gdi32.dll")]
+            public static extern IntPtr CreateCompatibleBitmap(IntPtr hDC, int nWidth,
+                int nHeight);
+            [DllImport("gdi32.dll")]
+            public static extern IntPtr CreateCompatibleDC(IntPtr hDC);
+            [DllImport("gdi32.dll")]
+            public static extern bool DeleteDC(IntPtr hDC);
+            [DllImport("gdi32.dll")]
+            public static extern bool DeleteObject(IntPtr hObject);
+            [DllImport("gdi32.dll")]
+            public static extern IntPtr SelectObject(IntPtr hDC, IntPtr hObject);
+        }
+
+        /// <summary>
+        /// Helper class containing User32 API functions
+        /// </summary>
+        private class User32
+        {
+            [StructLayout(LayoutKind.Sequential)]
+            public struct RECT
+            {
+                public int left;
+                public int top;
+                public int right;
+                public int bottom;
+            }
+            [DllImport("user32.dll")]
+            public static extern IntPtr GetDesktopWindow();
+            [DllImport("user32.dll")]
+            public static extern IntPtr GetWindowDC(IntPtr hWnd);
+            [DllImport("user32.dll")]
+            public static extern IntPtr ReleaseDC(IntPtr hWnd, IntPtr hDC);
+            [DllImport("user32.dll")]
+            public static extern IntPtr GetWindowRect(IntPtr hWnd, ref RECT rect);
+        }
+
+
+
+        [DllImport("user32.dll", EntryPoint = "FindWindow")]
+        private static extern IntPtr FindWindow(string lp1, string lp2);
+
+        [DllImport("user32.dll", ExactSpelling = true, CharSet = CharSet.Auto)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool SetForegroundWindow(IntPtr hWnd);
+       
+
+        public void GetCapture()
+        {
+
+            IntPtr handle = FindWindow("Window", "Cartes du Ciel - Chart_1");
+            ScreenCapture sc = new ScreenCapture();
+            Image img = sc.CaptureScreen();
+        //    sc.CaptureWindowToFile(handle, "C:\\atest3\\TestCapture.jpg", ImageFormat.Jpeg);
+        //    sc.CaptureWindowToFile(handle, @"C:\Users\Public\CDC_CamSimulator\CDC Camera Simulator\CDC Camera Simulator\bin\Debug\TestCapture.jpg", ImageFormat.Jpeg);
+       //     sc.CaptureWindowToFile(handle,  Path.Combine(fullPath, @"TestCapture.bmp"), ImageFormat.Bmp);
+            sc.CaptureWindowToFile(handle, Path.Combine(fullPath, @"SimCapture.jpg"), ImageFormat.Jpeg);
+        }
+    }
+}
