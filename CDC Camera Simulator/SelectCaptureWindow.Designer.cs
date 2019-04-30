215013ef02b4278251d40629939ac577df779fa6
ksipp01 <ksipp911@gmail.com>
2019-04-30 07:47:12 -0500

    SelectCaptureWindow was not prev committed

---
+++ CDC Camera Simulator/SelectCaptureWindow.Designer.cs
@@ -0,0 +1,53 @@
+﻿namespace ASCOM.SimCDC
+{
+    partial class SelectCaptureWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // SelectCaptureWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(518, 416);
+            this.ControlBox = false;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "SelectCaptureWindow";
+            this.Opacity = 0.2D;
+            this.Text = "SelectCaptureWindow";
+            this.TransparencyKey = System.Drawing.Color.White;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.SelectCaptureWindow_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
